06d3c8de859177cb36ffade9cbd1dfe0467fc4a7
Duriel7 <duriel@live.fr>
2026-02-13 13:48:24 +0100

    feat: abstract master controller

--- src/Controller/MasterController.php
+++ src/Controller/MasterController.php
@@ -2,5 +2,22 @@
 namespace Diablo\Controller;
 
 abstract class MasterController {
+    protected $twig;
 
+    public function __construct()
+    {
+        $loader = new \Twig\Loader\FilesystemLoader($_SERVER['DOCUMENT_ROOT'].'/../src/View');
+        $this->twig = new \Twig\Environment($loader, [
+            'cache' => $_SERVER['DOCUMENT_ROOT'].'/../var/cache'
+            ,'debug' => true
+        ]);
+        $this->twig->addExtension(new \Twig\Extension\DebugExtension());
+
+        $fileExist = new \Twig\TwigFunction('file_exists', function($fullFileName){
+            return file_exists($fullFileName);
+        });
+        $this->twig->addFunction($fileExist);
+
+        $this->twig->addGlobal('session', $_SESSION);
+    }
 }
