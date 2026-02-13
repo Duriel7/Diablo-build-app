b987f1d42fe41b270f73c5b495d1cece9ecda279
Duriel7 <duriel@live.fr>
2026-02-13 13:27:39 +0100

    feat: created database connection

--- src/Data/DataBaseConnection.php
+++ src/Data/DataBaseConnection.php
@@ -2,3 +2,33 @@
 namespace Diablo\Data;
 use PDO;
 use PDOException;
+
+class DataBaseConnection{
+    public static function getInstance(): PDO{
+        // Load environment variables
+        $dbhostname = $_ENV['DB_HOST'] ?? "mariadb:10.11";
+        $dbname = $_ENV['DB_NAME'] ?? "diablo";
+        $dbusername = $_ENV['DB_USERNAME'] ?? "diablo";
+        $dbpassword = $_ENV['DB_PASSWORD'] ?? null;
+        $dbport = $_ENV['DB_PORT'] ?? 3306;
+
+        // Create DSN string
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s;dbname=%s;charset=utf8',
+            $dbhostname, $dbport, $dbname
+        );
+        
+        // Attempt to create a PDO instance
+        try{
+            $instance = new PDO(
+                dsn: $dsn,
+                username: $dbusername,
+                password: $dbpassword
+            );
+
+            return $instance;
+        }catch (PDOException $error){
+            throw new \PDOException('Database connection failed because of: ' . $error->getMessage(), (int)$error->getCode());
+        }
+    }
+}
