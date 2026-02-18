e3326e4b51cffb587b8588b27e75b9e37c1eeaff
Duriel7 <duriel@live.fr>
2026-02-18 13:50:31 +0100

    fix: database connection reinforced

--- src/Data/DataBaseConnection.php
+++ src/Data/DataBaseConnection.php
@@ -4,31 +4,42 @@ use PDO;
 use PDOException;
 
 class DataBaseConnection{
-    public static function getInstance(): PDO{
-        // Load environment variables
-        $dbhostname = $_ENV['DB_HOST'] ?? "mariadb:10.11";
-        $dbname = $_ENV['DB_NAME'] ?? "diablo";
-        $dbusername = $_ENV['DB_USERNAME'] ?? "diablo";
-        $dbpassword = $_ENV['DB_PASSWORD'] ?? null;
-        $dbport = $_ENV['DB_PORT'] ?? 3306;
+    private static ?PDO $instance = null;
+    public static function getInstance(): PDO {
+        if (self::$instance === null) {
+            // Load environment variables
+            $dbhostname = $_ENV['DB_HOSTNAME'] ?? "db";
+            $dbname = $_ENV['DB_NAME'] ?? "diablo";
+            $dbusername = $_ENV['DB_USERNAME'] ?? "diablo";
+            $dbpassword = $_ENV['DB_PASSWORD'] ?? null;
+            $dbport = $_ENV['DB_PORT'] ?? 3306;
 
-        // Create DSN string
-        $dsn = sprintf(
-            'mysql:host=%s;port=%s;dbname=%s;charset=utf8',
-            $dbhostname, $dbport, $dbname
-        );
-        
-        // Attempt to create a PDO instance
-        try{
-            $instance = new PDO(
-                dsn: $dsn,
-                username: $dbusername,
-                password: $dbpassword
+            // Create DSN string
+            $dsn = sprintf(
+                'mysql:host=%s;port=%s;dbname=%s;charset=utf8mb4',
+                $dbhostname, $dbport, $dbname
             );
-
-            return $instance;
-        }catch (PDOException $error){
-            throw new \PDOException('Database connection failed because of: ' . $error->getMessage(), (int)$error->getCode());
+            
+            // Options for PDO connection
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
+            
+            // Attempt to create a PDO instance
+            try{
+                self::$instance = new PDO(
+                    $dsn,
+                    $dbusername,
+                    $dbpassword,
+                    $options
+                );
+            }catch (PDOException $error){
+                throw new \PDOException('Database connection failed because of: ' . $error->getMessage(), (int)$error->getCode());
+            }
         }
+        
+        return self::$instance;
     }
 }
