f7f487ce901d1b17301c57cb92dea6ea61847fb3
Duriel7 <duriel@live.fr>
2026-02-18 13:38:34 +0100

    WIP feat: user model

--- src/Model/User.php
+++ src/Model/User.php
@@ -1,3 +1,21 @@
 <?php
 namespace Diablo\Model;
+
+use DateTime;
 use Diablo\Database\DatabaseConnection;
+
+class User {
+    //Properties
+    private ?int  $Id = null;
+    private string $Nickname;
+    private ?string $Bio; //abstract about the user, can be empty
+    private \DateTime $RegisteredAt;
+    private string $City;
+    private ?float $latitude;
+    private ?float $longitude;
+    private string $Email;
+    private string $Password;
+    private string $Role;
+    private ?string $AvatarRepository;
+    private ?string $AvatarFileName;
+}
