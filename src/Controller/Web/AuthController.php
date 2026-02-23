412b27837d993fa1a336762f2a1d2d13a458eecf
Duriel7 <duriel@live.fr>
2026-02-23 13:15:02 +0100

    fix: files cleaning in web controller folder

--- src/Controller/Web/AuthController.php
+++ src/Controller/Web/AuthController.php
@@ -2,6 +2,6 @@
 namespace Diablo\Controller\Web;
 
 use Diablo\Controller\AbstractController;
-abstract class BuildController extends AbstractController {
+abstract class AuthController extends AbstractController {
     
 }
