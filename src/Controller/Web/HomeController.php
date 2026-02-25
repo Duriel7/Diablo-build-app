c51ed73be1a06ee7d93d987f783ff2d6388b5a40
Duriel7 <duriel@live.fr>
2026-02-25 17:47:54 +0100

    feat: added constructor in web home controller + enhanced index with show last builds

--- src/Controller/Web/HomeController.php
+++ src/Controller/Web/HomeController.php
@@ -2,10 +2,24 @@
 
 namespace Diablo\Controller\Web;
 
+use Diablo\Core\Request;
+use Diablo\Repository\BuildRepository;
+
 class HomeController extends AbstractWebController
 {
-    public function index(): void
-    {
-        $this->render('home/index.html.twig');
+    public function __construct(
+        Request $request,
+        private BuildRepository $buildRepository
+    ) {
+        parent::__construct($request);
+    }
+
+    public function index(): void{
+        //Limit = 3, Offset = 0
+        $latestBuilds = $this->buildRepository->SqlGetAllBuildsPaginated(3, 0);
+
+        $this->render('home/index.html.twig', [
+            'latestBuilds' => $latestBuilds
+        ]);
     }
 }
