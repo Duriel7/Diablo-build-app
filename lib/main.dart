f1b7c629e5cb4982638bc1cba54ee849164c3950
Duriel7 <duriel@live.fr>
2026-03-05 14:15:28 +0100

    fix: reversed bool changes for checks

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
+import 'package:diablo_build_app/pages/login_page.dart';
 import 'package:diablo_build_app/pages/register_page.dart';
-import 'package:diablo_build_app/services/user_service.dart';
 import 'package:flutter/material.dart';
 import 'package:diablo_build_app/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final userService = UserService();
-  final userData = await userService.getLocalUser();
-  final bool loggedIn = userData != null;
-
-  runApp(MyApp(isLoggedIn: loggedIn));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final bool isLoggedIn;
-  const MyApp({super.key, required this.isLoggedIn});
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     
@@ -26,18 +20,18 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFF1A1A1A), // equivalent of the --dark-bg in my CSS
         colorScheme: const ColorScheme.dark(
           primary: Color(0xFFC5A059), // equivalent of the --gold-accent in my CSS
-          secondary: Color(0xFF8B0000), // equivalent of the --diablo-red in my CSS
+          secondary: Color(0xFF660000), // equivalent of the --diablo-red in my CSS
         ),
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.black,
           titleTextStyle: TextStyle(color: Color(0xFFC5A059), fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
-      //home: const MyHomePage(title: 'Sanctuaire : Forge'),
-      home: isLoggedIn ? const MyHomePage(title: 'Sanctuaire : Forge') : const RegisterPage(),
+      home: const MyHomePage(title: 'Sanctuaire : Forge'),
       routes: {
-        '/register': (context) => const RegisterPage(),
         '/home': (context) => const MyHomePage(title: 'Sanctuaire : Forge'),
+        '/register': (context) => const RegisterPage(),
+        '/login': (context) => const LoginPage(),
       },
     );
   }
